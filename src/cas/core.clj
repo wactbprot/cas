1f48aaf2f8164bc49758d4bc2d71af6e9868a6e9
Thomas Bock <thomas.bock@ptb.de>
2022-12-07 09:45:11 +0100

    integrant start stop

--- src/cas/core.clj
+++ src/cas/core.clj
@@ -6,10 +6,32 @@
             [clj-http.client :as http]
             [clojure.java.io :as io]
             [compojure.core :refer [defroutes context GET POST]]
+            [integrant.core :as ig]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.session :refer [wrap-session]]
             [ring.middleware.params :refer [wrap-params]]
             [ring.util.response :refer [response redirect]]))
+
+
+(defonce system (atom {}))
+
+(def config {:db/couch {:opts
+                        {:basic-auth [(System/getenv "CAL_USR") (System/getenv "CAL_PWD")]
+                         ;; :body "{\"json\": \"input\"}"
+                         :headers {"X-Api-Version" "2"}
+                         :content-type :json
+                         :socket-timeout 1000      ;; in milliseconds
+                         :connection-timeout 1000  ;; in milliseconds
+                         :accept :json
+                         :query-params {}
+                         :pool {:threads 1 :default-per-route 1}}
+                        :prot "http"
+                        :host "localhost"
+                        :port 5984}
+
+             :server/jetty {:opts {:port 8080
+                                   :join? false}
+                            :backend (session-backend)}})
 
 ;; https://github.com/adambard/buddy-test/blob/master/src/buddy_test/app.clj
 
@@ -80,19 +102,35 @@
   (POST "/login/" [] post-login)
   (POST "/logout/" [] post-logout))
 
-(def backend (session-backend))
-(def my-app
-  (-> #'all-routes
-      (wrap-user)
-      (wrap-authentication backend)
-      (wrap-authorization backend)
-      (wrap-session)
-      (wrap-params)))
+(comment
+  (defn -main []
+                                        ; Init admin user
+    (create-user! {:username "admin" :password "1234"})
+    ( my-app {:port 8080})))
 
-(defn -main []
-  ; Init admin user
-  (create-user! {:username "admin" :password "1234"})
-  (run-jetty my-app {:port 8080}))
+
+(defmethod ig/init-key :db/couch [_ {:keys [prot host port] :as conf}]
+  (assoc conf :url (str prot "://" host ":" port "/")))
+
+(defmethod ig/init-key :server/jetty [_ {:keys [opts backend]}]
+  (run-jetty (-> #'all-routes
+                 (wrap-user)
+                 (wrap-authentication backend)
+                 (wrap-authorization backend)
+                 (wrap-session)
+                 (wrap-params))
+             opts))
+
+(defmethod ig/halt-key! :server/jetty [_ server]
+  (.stop server))
+
+(defn start []
+  (keys (reset! system (ig/init config))))
+
+(defn stop []
+  (ig/halt! @system)
+  (reset! system {}))
+
 
 (comment
 
