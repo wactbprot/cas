c285ed5930274a3069a12a4fc87325f0368911a5
Thomas Bock <thomas.bock@ptb.de>
2022-12-07 15:52:13 +0100

    half way to register

--- src/ccas/core.clj
+++ src/ccas/core.clj
@@ -3,9 +3,12 @@
             [buddy.auth.backends.session :refer [session-backend]]
             [buddy.auth.middleware :refer [wrap-authentication wrap-authorization]]
             [buddy.hashers :as hashers]
+            [buddy.auth :refer [authenticated? throw-unauthorized]]
             [clj-http.client :as http]
             [clojure.java.io :as io]
+            [clojure.data.json :as json]
             [compojure.core :refer [defroutes context GET POST]]
+            [compojure.route :refer [not-found]]
             [integrant.core :as ig]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.session :refer [wrap-session]]
@@ -28,12 +31,25 @@
                         :host "localhost"
                         :port 5984}
 
+             :get/register {:db (ig/ref :db/couch)
+                            :path "vl_db/_design/ccas/register.html"}
+
+             :post/register {:db (ig/ref :db/couch)
+                             :pwd-opts {:min-length 3}
+                             :allowed-users "vl_db/000_MAINTAINERS"}
+             
              :get/login {:db (ig/ref :db/couch)
                          :path "vl_db/_design/ccas/login.html"}
              
-             :routes/all {:get-login (ig/ref :get/login)}
+             :get/index {:db (ig/ref :db/couch)
+                         :path "vl_db/_design/ccas/login.html"}
+             
+             :routes/app {:get-register (ig/ref :get/register)
+                          :post-register (ig/ref :post/register)
+                          :get-login (ig/ref :get/login)
+                          :get-index (ig/ref :get/index)}
 
-             :server/app {:routes (ig/ref :routes/all)
+             :server/app {:routes (ig/ref :routes/app)
                           :backend (session-backend)}
              
              :server/jetty {:opts {:port 8080
@@ -71,16 +87,9 @@
                      (hashers/check password (:password-hash user)))
               (reduced user))) (vals @userstore)))
 
-(defn get-index [req]
-  (slurp (io/resource "public/index.html")))
-
-(defn get-admin [req]
-  (slurp (io/resource "public/admin.html")))
-
-(defn post-login [{{username "username" password "password"} :form-params
-                   session :session :as req}]
+(defn post-login [{{username "username" password "password"} :form-params session :session :as req}]
   (if-let [user (get-user-by-username-and-password username password)]
-    (assoc (redirect "/")
+    (assoc (redirect "/index/")
            :session (assoc session :identity (:id user)))
     (redirect "/login/")))
 
@@ -88,35 +97,64 @@
   (assoc (redirect "/login/")
          :session (dissoc session :identity)))
 
-(defn is-authenticated [{user :user :as req}]
-  (not (nil? user)))
-
 (defn wrap-user [handler]
   (fn [{user-id :identity :as req}]
     (handler (assoc req :user (get-user user-id)))))
 
-(defroutes admin-routes
-  (GET "/" [] get-admin))
+(defn get-allowed-users [{:keys [srv opts] :as db} path]
+  (-> (http/get (str srv path) opts)
+      :body
+      (json/read-str :key-fn keyword)
+      :Maintainers))
 
+(defn user-allowed? [vec-of-maps key value]
+  (->> vec-of-maps
+       (filterv (fn [m] (= value (key m))))
+       count
+       pos?))
+
+(defn passwds-ok? [pwd1 pwd2 {:keys [min-length] :as pwd-opts}]
+  (and (= pwd1 pwd2)
+       (<= min-length (count pwd1))))
+
+;; ## system up
 
 (defmethod ig/init-key :db/couch [_ {:keys [prot host port] :as conf}]
   (assoc conf :srv (str prot "://" host ":" port "/")))
 
-
 (defmethod ig/init-key :get/login [_ {:keys [path db]}]
   (fn [req]
-    (prn "call")
     (let [{srv :srv opts :opts} db]
       (-> (http/get (str srv path) opts) :body))))
 
-(defmethod ig/init-key :routes/all [_ {:keys [ini get-login] :as conf}]
+(defmethod ig/init-key :get/register [_ {:keys [path db]}]
+  (fn [req]
+    (let [{srv :srv opts :opts} db]
+      (-> (http/get (str srv path) opts) :body))))
+
+(defmethod ig/init-key :post/register [_ {:keys [db allowed-users pwd-opts]}]
+  (fn [{{email "email" pwd1 "password1" pwd2 "password2"} :form-params session :session :as req}]
+    (let [allowed-users (get-allowed-users db allowed-users)]
+      (if (and (user-allowd? allowed-users :email email)
+               (pwds-ok? pwd1 pwd2 pwd-opts))
+        "yey"
+        "ney"))))
+
+(defmethod ig/init-key :get/index [_ {:keys [path db]}]
+  (fn [req]
+    (if-not (authenticated? req)
+      (throw-unauthorized)
+      (str "<h1>hello auth</h1>" path))))
+
+(defmethod ig/init-key :routes/app [_ {:keys [get-login get-index get-register post-register] :as conf}]
   (defroutes all-routes
-    (context "/admin" []
-             (restrict admin-routes {:handler identity}))
-    (GET "/" [] get-index)
+    (GET "/index/" [] get-index)
     (GET "/login/" [] get-login)
+    (GET "/register/" [] get-register)
+    (POST "/register/" [] post-register)
     (POST "/login/" [] post-login)
-    (POST "/logout/" [] post-logout)))
+    (POST "/logout/" [] post-logout)
+    (not-found "<h1>not found</h1>")))
 
 (defmethod ig/init-key :server/app [_ {:keys [backend routes]}]
   (-> routes
@@ -126,12 +164,14 @@
       (wrap-session)
       (wrap-params)))
 
+;; ## system down
 (defmethod ig/init-key :server/jetty [_ {:keys [opts app]}]
   (run-jetty app  opts))
 
 (defmethod ig/halt-key! :server/jetty [_ server]
   (.stop server))
 
+;; ## start stop
 (defn start []
   (keys (reset! system (ig/init config))))
 
@@ -139,16 +179,24 @@
   (ig/halt! @system)
   (reset! system {}))
 
+;; ## helper functions
+
 (defn app [] (-> system deref :server/app))
 
+
+;; ## playground
 (comment
   ((app) {:request-method :post :uri "/login/" :body "username=admin&password=1234"})
   ((app) {:request-method :get :uri "/admin/"
-           :headers {"cookie" "ring-session=5c39d06a-156d-401f-ae1c-f86a2ca717d6"}
-           }))
+          :headers {"cookie" "ring-session=5c39d06a-156d-401f-ae1c-f86a2ca717d6"}}))
 
 (comment
-  (defn -main []
-                                        ; Init admin user
-    (create-user! {:username "admin" :password "1234"})
-    ( my-app {:port 8080})))
+  (create-user! {:username "admin" :password "1234"}))
+
+(comment
+  (def vec-of-maps
+    (get-allowed-users (:db/couch @system) "vl_db/000_MAINTAINERS"))
+  (user-allowed? vec-of-maps :email "Thomas.Bock@ptb.de"))
+
+(comment
+  (passwds-ok? "123" "123" {:min-length 3}))
